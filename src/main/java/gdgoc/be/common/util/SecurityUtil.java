93747d8d3c8e8acd76279817a9bcd6a346ba8802
Grow22 <uuu1856@naver.com>
2026-02-21 17:55:44 +0900

    feat: SecurityUtil Class 생성

---
+++ src/main/java/gdgoc/be/common/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package gdgoc.be.common.util;
+
+import gdgoc.be.exception.BusinessErrorCode;
+import gdgoc.be.exception.BusinessException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtil {
+    public static String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new BusinessException(BusinessErrorCode.AUTH_REQUIRED);
+        }
+
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            return ((UserDetails) authentication.getPrincipal()).getUsername();
+        }
+        return authentication.getPrincipal().toString();
+    }
+}
