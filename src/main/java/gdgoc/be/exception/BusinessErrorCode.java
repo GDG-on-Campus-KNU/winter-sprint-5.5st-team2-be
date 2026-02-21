650cee7eff03dc4ae162e85f44e997d8df7065da
Grow22 <uuu1856@naver.com>
2026-02-21 17:38:56 +0900

    refactor: forbidden 예외 코드 추가

--- src/main/java/gdgoc/be/exception/BusinessErrorCode.java
+++ src/main/java/gdgoc/be/exception/BusinessErrorCode.java
@@ -19,7 +19,8 @@ public enum BusinessErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문 내역을 찾을 수 없습니다."),
 
     EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN,"접근 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
