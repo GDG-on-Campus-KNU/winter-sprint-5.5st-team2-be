69ff7c70ed6aebc30a702a6eb3ac895894cfdd64
Grow22 <uuu1856@naver.com>
2026-02-21 23:35:47 +0900

    feat: Admin 검증 Filter 추가

--- src/main/java/gdgoc/be/security/SecurityConfig.java
+++ src/main/java/gdgoc/be/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
