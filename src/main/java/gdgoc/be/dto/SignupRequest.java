988f4d9cb759801d9ce9604b9822fa3b0b9bf74a
Grow22 <uuu1856@naver.com>
2026-02-23 17:04:29 +0900

    refactor: SignupRequest record 타입으로 변경

--- src/main/java/gdgoc/be/dto/SignupRequest.java
+++ src/main/java/gdgoc/be/dto/SignupRequest.java
@@ -2,24 +2,17 @@ package gdgoc.be.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class SignupRequest {
-    @NotBlank(message = "이메일은 필수입니다.")
-    @Email(message = "올바른 이메일 형식이 아닙니다.")
-    private String email;
-
-    @NotBlank(message = "비밀번호는 필수입니다.")
-    private String password;
-
-    @NotBlank(message = "이름은 필수입니다.")
-    private String name;
-
-    String address;
+public record SignupRequest(
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        String email,
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        String password,
+        @NotBlank(message = "이름은 필수입니다.")
+        String name,
+        String address
+) {
 
     public SignupRequest(String email, String password, String name, String address) {
         this.email = email;
