782a6b9841b4ffa27b0e50854a28614a87e0e06b
Grow22 <uuu1856@naver.com>
2026-02-24 12:44:16 +0900

    refactor: 변수 이름 변경

--- src/main/java/gdgoc/be/dto/order/OrderResponse.java
+++ src/main/java/gdgoc/be/dto/order/OrderResponse.java
@@ -24,13 +24,13 @@ public record OrderResponse(
 ) {
 
     public static OrderResponse from(Order order) {
-        List<OrderItemResponse> itemNum = order.getOrderItems().stream()
+        List<OrderItemResponse> itemResponses = order.getOrderItems().stream()
                 .map(OrderItemResponse::from)
                 .toList();
 
-        String repName = itemNum.get(0).productName();
-        if (itemNum.size() > 1) {
-            repName += " 외 " + (itemNum.size() - 1) + "건";
+        String repName = itemResponses.get(0).productName();
+        if (itemResponses.size() > 1) {
+            repName += " 외 " + (itemResponses.size() - 1) + "건";
         }
 
         return OrderResponse.builder()
